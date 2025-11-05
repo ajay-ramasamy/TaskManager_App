f386177f5c399848f0a6e2f1aad419f59412d561
ajay ramasamy j <ajayem1222@gmail.com>
2025-11-05 14:29:44 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // ONLY for development
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
